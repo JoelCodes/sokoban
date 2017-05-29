d284445575aaf8384e824014c4832486005b9d2a
JoelCodes <amnshinness@yahoo.com>
2017-05-28 23:06:13 -0700

    Added immutable.js, wrote more turning logic and test descriptions.

--- reducers/level.js
+++ reducers/level.js
@@ -1,5 +1,5 @@
 const { createReducer } = require('redux-create-reducer');
-
+const { Map, List } = require('immutable');
 
 const NOT_SET_UP = 'NOT_SET_UP';
 const READY = 'READY';
@@ -13,77 +13,89 @@ const PLAYER = 'PLAYER';
 const GOAL = 'GOAL';
 const UP = 'UP';
 const DOWN = 'DOWN';
+const SPACE = 'SPACE';
+const LEFT = 'LEFT';
+const RIGHT = 'RIGHT';
 
+const initialState = new Map({
+  status: NOT_SET_UP,
+});
 module.exports = {
-  level: createReducer({ blocks: [], status: NOT_SET_UP, player: {}, grid: [[]] }, {
+  level: createReducer(initialState, {
     [SETUP](state, { grid }) {
-      if (state.status !== NOT_SET_UP) return state;
-      const filterGrid = (symbol) => {
-        const filterInRow = (agg, r) => {
-          if (r >= grid.length) return agg;
-          const row = grid[r];
-          const filterInCell = (subAgg, c) => {
-            if (c >= row.length) return subAgg;
-            if (row[c] === symbol) {
-              return filterInCell(subAgg.concat({ r, c }), c + 1);
-            }
-            return filterInCell(subAgg, c + 1);
-          };
+      if (state.get('status') !== NOT_SET_UP) return state;
+      const filterGrid = symbol => grid.flatMap((row, r) => row.reduce((agg, cell, c) => {
+        if (cell === symbol) {
+          return agg.push(new Map({ r, c }));
+        }
+        return agg;
+      }, new List()));
 
-          return filterInRow(filterInCell(agg, 0), r + 1);
-        };
-        return filterInRow([], 0);
-      };
       const find = (symbol) => {
-        const findInRow = (r) => {
-          if (r >= grid.length) return undefined;
-          const row = grid[r];
-          const findInCell = (c) => {
-            if (c >= row.length) return undefined;
-            if (row[c] === symbol) return { r, c };
-            return findInCell(c + 1);
+        const searchRows = (remainingRows, r) => {
+          if (remainingRows.size === 0) return undefined;
+          const searchCells = (remainingCells, c) => {
+            if (remainingCells.size === 0) return undefined;
+            if (remainingCells.first() === symbol) return new Map({ r, c });
+            return searchCells(remainingCells.slice(1), c + 1);
           };
-          return findInCell(0) || findInRow(r + 1);
+          const result = searchCells(remainingRows.first(), 0);
+          return result || searchRows(remainingRows.slice(1), r + 1);
         };
-        return findInRow(0);
+        return searchRows(grid, 0);
       };
-      const player = find(PLAYER);
-      player.direction = NORTH;
+      const player = find(PLAYER).set('direction', NORTH);
       const blocks = filterGrid(BLOCK);
       const goals = filterGrid(GOAL);
-      return {
+      return new Map({
         status: READY,
         blocks,
         player,
         goals,
         grid,
-      };
+      });
     },
     [UP](state) {
-      if (state.status !== READY) return state;
-      if (state.player.direction === EAST) {
-        const player = Object.assign({}, state.player, { direction: NORTH });
-        return Object.assign({}, state, { player });
+      if (state.get('status') !== READY) return state;
+      const direction = state.getIn(['player', 'direction']);
+      if (direction === EAST) {
+        return state.setIn(['player', 'direction'], NORTH);
       }
-      if (state.player.direction === SOUTH) {
-        const player = Object.assign({}, state.player, { direction: WEST });
-        return Object.assign({}, state, { player });
+      if (direction === SOUTH) {
+        return state.setIn(['player', 'direction'], WEST);
       }
       return state;
     },
     [DOWN](state) {
-      if (state.status !== READY) return state;
-      if (state.player.direction === NORTH) {
-        return Object.assign({}, state, {
-          player: Object.assign({}, state.player, { direction: EAST }),
-        });
+      if (state.get('status') !== READY) return state;
+      const direction = state.getIn(['player', 'direction']);
+      if (direction === NORTH) {
+        return state.setIn(['player', 'direction'], EAST);
       }
-      if (state.player.direction === WEST) {
-        return Object.assign({}, state, {
-          player: Object.assign({}, state.player, {
-            direction: SOUTH,
-          }),
-        });
+      if (direction === WEST) {
+        return state.setIn(['player', 'direction'], SOUTH);
+      }
+      return state;
+    },
+    [LEFT](state) {
+      if (state.get('status') !== READY) return state;
+      const direction = state.getIn(['player', 'direction']);
+      if (direction === NORTH) {
+        return state.setIn(['player', 'direction'], WEST);
+      }
+      if (direction === EAST) {
+        return state.setIn(['player', 'direction'], SOUTH);
+      }
+      return state;
+    },
+    [RIGHT](state) {
+      if (state.get('status') !== READY) return state;
+      const direction = state.getIn(['player', 'direction']);
+      if (direction === WEST) {
+        return state.setIn(['player', 'direction'], NORTH);
+      }
+      if (direction === SOUTH) {
+        return state.setIn(['player', 'direction'], EAST);
       }
       return state;
     },
@@ -100,4 +112,7 @@ module.exports = {
   GOAL,
   UP,
   DOWN,
+  LEFT,
+  RIGHT,
+  SPACE,
 };
--- test/level.js
+++ test/level.js
@@ -1,10 +1,11 @@
 /* eslint-env mocha */
 const expect = require('chai').expect;
+const { fromJS } = require('immutable');
 const {
   level,
   NOT_SET_UP, READY, FINISHED,
   NORTH, WEST, EAST, SOUTH,
-  SETUP, UP, DOWN,
+  SETUP, UP, DOWN, LEFT, RIGHT,
   BLOCK, PLAYER, SPACE, GOAL,
 } = require('../reducers/level');
 
@@ -12,33 +13,31 @@ describe('#level(state, action)', () => {
   describe('Initial State And Level Setup', () => {
     it('returns the correct initial state', () => {
       const initialState = level(undefined, {});
-      expect(initialState).to.deep.eq({
+      expect(initialState.toJS()).to.deep.eq({
         status: NOT_SET_UP,
-        grid: [[]],
-        blocks: [],
-        player: {},
       });
     });
     describe('{type: SETUP, grid: block[][]}', () => {
       it('does nothing if the level status is other than NOT_SET_UP', () => {
-        const ready = { status: READY };
-        expect(level(ready, { type: SETUP, grid: [[]] })).to.eq(ready);
-        const finished = { status: FINISHED };
-        expect(level(finished, { type: SETUP, grid: [[]] })).to.eq(finished);
+        const ready = fromJS({ status: READY });
+        expect(level(ready, { type: SETUP, grid: fromJS([[]]) })).to.eq(ready);
+        const finished = fromJS({ status: FINISHED });
+        expect(level(finished, { type: SETUP, grid: fromJS([[]]) })).to.eq(finished);
       });
       it('sets up the grid, player position, and blocks', () => {
         const initialState = level(undefined, {});
-        const grid = [[GOAL, SPACE, BLOCK, PLAYER]];
+        const grid = fromJS([[GOAL, SPACE, BLOCK, PLAYER]]);
         const setupState = level(initialState, {
           type: SETUP,
           grid,
         });
-        expect(setupState).to.deep.eq({
+
+        expect(setupState.toJS()).to.deep.eq({
           status: READY,
           blocks: [{ r: 0, c: 2 }],
           player: { r: 0, c: 3, direction: NORTH },
           goals: [{ r: 0, c: 0 }],
-          grid,
+          grid: grid.toJS(),
         });
       });
     });
@@ -47,47 +46,49 @@ describe('#level(state, action)', () => {
   describe('Player Movement', () => {
     describe('{type: UP}', () => {
       it('does nothing if status is not READY', () => {
-        const notReady = {
+        const notReady = fromJS({
           status: NOT_SET_UP,
           player: { direction: SOUTH },
-        };
+        });
 
         expect(level(notReady, { type: UP })).to.eq(notReady);
-        const finished = {
+
+        const finished = fromJS({
           status: FINISHED,
           player: { direction: SOUTH },
-        };
+        });
+
         expect(level(finished, { type: UP })).to.eq(finished);
       });
       it('does nothing if the player is already facing NORTH or WEST', () => {
-        const alreadyNorth = {
+        const alreadyNorth = fromJS({
           status: READY,
           player: { direction: NORTH },
-        };
+        });
         expect(level(alreadyNorth, { type: UP })).to.eq(alreadyNorth);
 
-        const alreadyWest = {
+        const alreadyWest = fromJS({
           status: READY,
           player: { direction: WEST },
-        };
+        });
         expect(level(alreadyWest, { type: UP })).to.eq(alreadyWest);
       });
       it('sets player direction to NORTH if the player is facing EAST', () => {
-        const isEast = {
+        const isEast = fromJS({
           status: READY,
           player: { direction: EAST },
-        };
-        expect(level(isEast, { type: UP })).to.deep.eq({
+        });
+        expect(level(isEast, { type: UP }).toJS()).to.deep.eq({
           status: READY,
           player: { direction: NORTH },
         });
       });
       it('sets player direction to WEST if the player is facing SOUTH', () => {
-        const isSouth = {
+        const isSouth = fromJS({
           status: READY,
           player: { direction: SOUTH },
-        };
-        expect(level(isSouth, { type: UP })).to.deep.eq({
+        });
+        expect(level(isSouth, { type: UP }).toJS()).to.deep.eq({
           status: READY,
           player: { direction: WEST },
         });
@@ -95,63 +96,137 @@ describe('#level(state, action)', () => {
     });
     describe('{type: DOWN}', () => {
       it('does nothing if status is not READY', () => {
-        const notReady = {
+        const notReady = fromJS({
           status: NOT_SET_UP,
           player: { direction: NORTH },
-        };
-
+        });
         expect(level(notReady, { type: DOWN })).to.eq(notReady);
-        const finished = {
+
+        const finished = fromJS({
           status: FINISHED,
           player: { direction: NORTH },
-        };
+        });
         expect(level(finished, { type: DOWN })).to.eq(finished);
       });
       it('does nothing if the player is already facing EAST or SOUTH', () => {
-        const alreadyEast = {
+        const alreadyEast = fromJS({
           status: READY,
           player: { direction: EAST },
-        };
+        });
         expect(level(alreadyEast, { type: DOWN })).to.eq(alreadyEast);
 
-        const alreadySouth = {
+        const alreadySouth = fromJS({
           status: READY,
           player: { direction: SOUTH },
-        };
+        });
         expect(level(alreadySouth, { type: DOWN })).to.eq(alreadySouth);
       });
       it('sets player direction to EAST if the player is facing NORTH', () => {
-        const isNorth = {
+        const isNorth = fromJS({
           status: READY,
           player: { direction: NORTH },
-        };
-        expect(level(isNorth, { type: DOWN })).to.deep.eq({
+        });
+        expect(level(isNorth, { type: DOWN }).toJS()).to.deep.eq({
           status: READY,
           player: { direction: EAST },
         });
       });
       it('sets player direction to SOUTH if the player is facing WEST', () => {
-        const isWest = {
+        const isWest = fromJS({
           status: READY,
           player: { direction: WEST },
-        };
-        expect(level(isWest, { type: DOWN })).to.deep.eq({
+        });
+        expect(level(isWest, { type: DOWN }).toJS()).to.deep.eq({
           status: READY,
           player: { direction: SOUTH },
         });
       });
     });
     describe('{type: LEFT}', () => {
-      it('does nothing if status is not READY');
-      it('turns a NORTH facing player WEST');
-      it('turns an EAST facing player SOUTH');
+      it('does nothing if status is not READY', () => {
+        const notReady = fromJS({
+          status: NOT_SET_UP,
+          player: { direction: NORTH },
+        });
+        expect(level(notReady, { type: LEFT })).to.eq(notReady);
+
+        const finished = fromJS({
+          status: FINISHED,
+          player: { direction: NORTH },
+        });
+        expect(level(finished, { type: LEFT })).to.eq(finished);
+      });
+      it('turns a NORTH facing player WEST', () => {
+        const north = fromJS({
+          status: READY,
+          player: { direction: NORTH },
+        });
+        expect(level(north, { type: LEFT }).toJS()).to.deep.eq({
+          status: READY,
+          player: { direction: WEST },
+        });
+      });
+      it('turns an EAST facing player SOUTH', () => {
+        const east = fromJS({
+          status: READY,
+          player: { direction: EAST },
+        });
+        expect(level(east, { type: LEFT }).toJS()).to.deep.eq({
+          status: READY,
+          player: { direction: SOUTH },
+        });
+      });
       it('moves a SOUTH facing player further SOUTH');
       it('moves a WEST facing player further WEST');
+      it('prevents movement into walls');
+      it('moves a box straight ahead if it has an opening on the other side');
+      it('does not move a box whose path is blocked');
+      it('does not move adjacent boxes not directly ahead');
+      it('permits movement onto a goal');
+      it('recognizes a victory condition');
     });
     describe('{type: RIGHT}', () => {
-      it('does nothing if status is not READY');
-      it('turns a WEST facing player NORTH');
-      it('turns a SOUTH facing player EAST');
+      it('does nothing if status is not READY', () => {
+        const notReady = fromJS({
+          status: NOT_SET_UP,
+          player: { direction: WEST },
+        });
+        expect(level(notReady, { type: RIGHT })).to.eq(notReady);
+
+        const finished = fromJS({
+          status: FINISHED,
+          player: { direction: WEST },
+        });
+        expect(level(finished, { type: RIGHT })).to.eq(finished);
+      });
+      it('turns a WEST facing player NORTH', () => {
+        const west = fromJS({
+          status: READY,
+          player: {
+            direction: WEST,
+          },
+        });
+        expect(level(west, { type: RIGHT }).toJS()).to.deep.eq({
+          status: READY,
+          player: {
+            direction: NORTH,
+          },
+        });
+      });
+      it('turns a SOUTH facing player EAST', () => {
+        const south = fromJS({
+          status: READY,
+          player: {
+            direction: SOUTH,
+          },
+        });
+        expect(level(south, { type: RIGHT }).toJS()).to.deep.eq({
+          status: READY,
+          player: {
+            direction: EAST,
+          },
+        });
+      });
       it('moves a NORTH facing player further NORTH');
       it('moves an EAST facing player further EAST');
     });
